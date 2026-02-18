3fee0574b1b993e89f436f2e71f6cfad2875804c
Gustavo Lucas <gustavolucas1502@hotmail.com>
2026-02-18 20:44:28 -0300

    added toggle button + fixes

--- novaCoin.js
+++ novaCoin.js
@@ -1,3 +1,21 @@
+// Get text elements
+const balanceEl = document.getElementById("coinBalance");
+const perClickEl = document.getElementById("coinsPerClick");
+const minersEl = document.getElementById("autoMiners");
+const totalMinedEl = document.getElementById("totalMined");
+const totalClicksEl = document.getElementById("totalClicks");
+
+// Get button elements
+const statsButton = document.getElementById("statsButton");
+const statsTable = document.getElementById("statsTable");
+const coinButton = document.getElementById("coinButton");
+const upgradeButton1 = document.getElementById("upgradeButton1");
+const upgradeButton2 = document.getElementById("upgradeButton2");
+
+// Set audio
+const coinAudio = new Audio("SFX/coin-click.mp3");
+coinAudio.volume = 0.3;
+
 if (localStorage.length === 0){ 
     var gameStats = {
         novaCoinsBalance: 0,
@@ -28,8 +46,7 @@ if (localStorage.length === 0){
             amount: 0,
             speed: 1000
         }
-        
-    ]
+    ];
 
 } else {
     var gameStats = JSON.parse(localStorage.getItem("novaCoinSave"));
@@ -41,11 +58,27 @@ if (localStorage.length === 0){
 function render() {
     balanceEl.textContent = gameStats.novaCoinsBalance + " NC";
     perClickEl.textContent = gameStats.coinsPerClick;
-    minersEl.textContent = entities[0].amount; // sensitive
+    minersEl.textContent = entities[0].amount; // entities[index] may cause bugs eventually
     totalMinedEl.textContent = gameStats.totalMined;
     totalClicksEl.textContent = gameStats.totalClicks;
     upgradeButton1.textContent = upgrades[0].cost + " NC";
     upgradeButton2.textContent = upgrades[1].cost + " NC";
+}
+
+function renderBalance() {
+    balanceEl.textContent = gameStats.novaCoinsBalance + " NC";
+    totalMinedEl.textContent = gameStats.totalMined;
+}
+
+function handleClickUpgrade() {
+    const upgrade = upgrades[0];
+    if (gameStats.novaCoinsBalance >= upgrade.cost) {
+        gameStats.novaCoinsBalance -= upgrade.cost;
+
+        applyUpgradeStats(0);
+        upgradeButton1.textContent = `${upgrade.cost} NC`;
+        render();
+    }
 }
 
 function handleEntitiesUpgrade(index) {
@@ -54,20 +87,24 @@ function handleEntitiesUpgrade(index) {
     if (gameStats.novaCoinsBalance >= upgrade.cost) {
         gameStats.novaCoinsBalance -= upgrade.cost;
 
-        upgradeStats(upgrade);
-        let newCost = upgrade.cost = Math.floor(upgrade.cost * 1.32);
+        applyUpgradeStats(index);
+        upgrade.cost = Math.floor(upgrade.cost * 1.32);
         upgrade.level += 1;
 
         const upgradeButton = document.querySelector(`[value="${index}"]`);
-        upgradeButton.textContent = `${newCost} NC`;
+        upgradeButton.textContent = `${upgrade.cost} NC`;
         render();
     }
 }
 
-function upgradeStats(upgrade) {
+function applyUpgradeStats(upgradeIndex) {
+    let upgrade = upgrades[upgradeIndex];
+
     switch (upgrade.id) {
         case "clickBoost":
-            gameStats.coinsPerClick += 1;
+            gameStats.coinsPerClick += 1; // overall stat
+            upgrade.level += 1;
+            upgrade.cost = Math.floor(upgrade.cost * 1.32);
             return;
             
         case "autoMiner":
@@ -78,14 +115,33 @@ function upgradeStats(upgrade) {
     }
 }
 
+function playCoinSound() {
+    coinAudio.currentTime = 0;
+    coinAudio.play();
+}
+
 function mineCoins() {
     gameStats.novaCoinsBalance += gameStats.coinsPerClick;
     gameStats.totalMined += gameStats.coinsPerClick;
     gameStats.totalClicks += 1
-    render();
 
-    coinAudio.currentTime = 0;
-    coinAudio.play();
+    render();
+    playCoinSound();
+}
+
+function autoMine() { // (change in AutoMiner Upgrades update)
+    let upgradesMultiplier = 1; // temporary value 
+    let coinMiner = entities[0]; // temporary value
+    let frequency = coinMiner.speed;
+    let coinsPerFrequency = coinMiner.amount * upgradesMultiplier;
+
+    if (intervalID !== null) clearInterval(intervalID);
+    
+    intervalID = setInterval(() => {
+        gameStats.novaCoinsBalance += coinsPerFrequency;
+        gameStats.totalMined += coinsPerFrequency;
+        renderBalance();
+    }, frequency);
 }
 
 function saveGame() {
@@ -94,39 +150,14 @@ function saveGame() {
         localStorage.setItem("entitiesSave", JSON.stringify(entities));
 }
 
-function autoMine() {
-    let upgradesMultiplier = 1;
-    let index = entities.findIndex(entity => entity.id === "autoMiner");
-    let coinMine = entities[index];
-
-    let frequency = coinMine.speed;
-    let coinsPerFrequency = coinMine.amount * upgradesMultiplier;
-
-    if (intervalID !== null) {
-        clearInterval(intervalID);
+function toggleStatsTable() {
+    statsTable.hidden = !statsTable.hidden;
+    if (statsTable.hidden) {
+        statsButton.textContent = "⏶";
+    } else {
+        statsButton.textContent = "⏷";
     }
-    intervalID = setInterval(() => {
-        gameStats.novaCoinsBalance += coinsPerFrequency;
-        renderBalance();
-    }, frequency);
-
 }
-
-function renderBalance() {
-    balanceEl.textContent = gameStats.novaCoinsBalance + " NC";
-}
-
-// Get Elements 
-const balanceEl = document.getElementById("coinBalance");
-const perClickEl = document.getElementById("coinsPerClick");
-const minersEl = document.getElementById("autoMiners");
-const totalMinedEl = document.getElementById("totalMined");
-const totalClicksEl = document.getElementById("totalClicks");
-const coinButton = document.getElementById("coinButton");
-const upgradeButton1 = document.getElementById("upgradeButton1");
-const upgradeButton2 = document.getElementById("upgradeButton2");
-const coinAudio = new Audio("SFX/coin-click.mp3");
-coinAudio.volume = 0.3;
 
 
 render();
@@ -136,6 +167,7 @@ if (entities[0].amount > 0) {
     autoMine();
 }
 
+statsButton.addEventListener("click", toggleStatsTable);
 coinButton.addEventListener("click", mineCoins);
-upgradeButton1.addEventListener("click", () => handleEntitiesUpgrade(upgradeButton1.value));
+upgradeButton1.addEventListener("click", () => handleClickUpgrade());
 upgradeButton2.addEventListener("click", () => handleEntitiesUpgrade(upgradeButton2.value));
